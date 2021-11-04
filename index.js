7a404e05ffe8778107a427924680a6c141b1a11c
Gregory Banks <gbanks1438@gmail.com>
2021-11-04 09:28:04 -0400

    commit tree 5d452cfa11051bc1aaaa9105fb44c3406fa494bf

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(names, event) {
+    const thankYou = [];
+        for (let a = 0; a < names.length; a++) {
+            thankYou.push(`Thank you, ${names[a]}, for the wonderful ${event} gift!`)
+        }
+    return thankYou;
+}
+
+function countDown(init = 10) {
+    while (init >= 0) {
+        console.log(init--);
+    }
+}
